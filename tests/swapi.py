366664f5090ad27da88715e3036d3b9015d3fb1e
Aleksandr.Abramovich <hause.abramovi44@gmail.com>
2025-11-07 22:13:48 +0300

    - check test

--- tests/swapi.py
+++ tests/swapi.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from pathlib import Path
 
 class APIRequester():
     
@@ -39,12 +40,28 @@ class APIRequester():
 class SWRequester(APIRequester):
      
     def get_sw_categories(self):
-        result = self.get()
-        dict_result = json.loads(result.text)
-        print(dict_result)
-        for category, url in dict_result.items():
-            print(f"Категория: {category}")
-            print(f"URL: {url}\n")
+        if self.base_url.endswith('/'):
+            url = self.base_url
+        else:
+            url = f'{self.base_url}/'
+            
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                dict_result = json.loads(response.text)
+                print(dict_result)
+                for category, url in dict_result.items():
+                    print(f"Категория: {category}")
+                    print(f"URL: {url}\n")
+                return dict_result.keys()
+            else:
+                return f'<ошибка на сервере: {response.status_code}>'
+        except requests.ConnectionError:
+            return 'сетевая ошибка'
+        except requests.RequestException:
+            return 'ошибка соединения'
+        except json.JSONDecodeError:
+            return "Ошибка декодирования JSON"
 
      
     def get_sw_info(self, sw_type):
@@ -70,9 +87,13 @@ class SWRequester(APIRequester):
             return 'ошибка соединения'
 
     
-    def save_sw_data(self):
-        pass
+def save_sw_data():
+    Path("data").mkdir(exist_ok=True)
+    objectSw = SWRequester('https://swapi.dev/api')
+    categories = objectSw.get_sw_categories()
+    for category in list(categories):
+        with open(f'data/{category}.txt', 'w', encoding='utf-8') as file:
+            text = objectSw.get_sw_info(category)
+            file.write(text)    
 
-if __name__ == '__main__':
-    objectSw = SWRequester('https://swapi.dev/api/')
-    objectSw.get_sw_categories()
+
